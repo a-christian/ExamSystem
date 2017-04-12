8e4c34c3c7469fcb5fe00ef8ecc088de9ff07fda
ac986 <austinc726@gmail.com>
2017-04-11 21:08:18 -0400

    SQL login credentials hidden

--- Connect.php
+++ Connect.php
@@ -1,10 +1,10 @@
 <?php
    
-   $connection = mysqli_connect("sql1.njit.edu","ac482","Z498qJCz");
+   $connection = mysqli_connect("sql1.njit.edu","xxxx","xxxx");
         if(!$connection) {
             die("Couldn't connect to SQL server.");
         }
-   $SQLdb = mysqli_select_db($connection, "ac482");
+   $SQLdb = mysqli_select_db($connection, "xxxx");
         if(!$SQLdb) {
             die("Couldn't open the database");
         }
