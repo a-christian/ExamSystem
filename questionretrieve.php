319e9960db41b895dac875c086e003e7dbf0fbac
Austin <austinc726@gmail.com>
2017-04-14 16:13:19 -0400

    now pulling info as 2d array

--- questionretrieve.php
+++ questionretrieve.php
@@ -3,44 +3,44 @@
     
     
   include 'Connect.php';
+  
  
+
   $getQuestions = "SELECT * FROM QuestionBank";
+  
+
+  
   
   $result = mysqli_query($connection, $getQuestions);
   
   $numrows = mysqli_num_rows($result);
 
+  $exquest = [];
+      
+  $exquest["questcount"] = $numrows;
+
+
+          while($row = mysqli_fetch_assoc($result)) {
+			array_push($exquest, [
+			
+                        array($row['question']),
+			array($row['pointValue']),
+			array($row['category']),
+			array($row['difficulty']),
+			array($row['case1']),
+			array($row['case2']),
+			array($row['case3']),
+			array($row['case4']),
+			array($row['answer1']),
+			array($row['answer2']),
+			array($row['answer3']),
+			array($row['answer4'])
+			
+            ]);
+	 }     
   
-$i = 0;
-	   
-      //$exquest = array();
-$exquest["questcount"] = $numrows;
-
-
-          while($row = mysqli_fetch_assoc($result)){
-
-  
-            $exquest["question $i"] = $row['question'];
-			$exquest["points $i"] = $row['pointValue'];
-			$exquest["difficulty $i"] = $row['difficulty'];
-			$exquest["case1 $i"] = $row['case1'];
-			$exquest["case2 $i"] = $row['case2'];
-			$exquest["case3 $i"] = $row['case3'];
-			$exquest["case4 $i"] = $row['case4'];
-			$exquest["answer1 $i"] = $row['answer1'];
-			$exquest["answer2 $i"] = $row['answer2'];
-			$exquest["answer3 $i"] = $row['answer3'];
-			$exquest["answer4 $i"] = $row['answer4'];			
-            $i++;
-			      
-
-          }    
-  
-          echo json_encode($exquest);
-		  
-		 
-		  
-
-
-              mysqli_close($connection);
+			echo json_encode($exquest);
+ 
+        mysqli_close($connection);
 ?>  
+
