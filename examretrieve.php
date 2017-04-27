b8c4731adf202c861f5d2c3bb077fa1b00fe3419
Austin <austinc726@gmail.com>
2017-04-27 00:15:37 -0400

    pulling exam and test cases/ans

--- examretrieve.php
+++ examretrieve.php
@@ -4,7 +4,9 @@
   include 'Connect.php';
   
 
-$getExam = "SELECT examquestion,examid FROM Exam";
+$getExam = "SELECT examid, examquestion FROM Exam";
+
+
 
 $result = mysqli_query($connection, $getExam);
   
@@ -22,13 +24,46 @@ $result = mysqli_query($connection, $getExam);
     
 
           }
-             
-  
-          echo json_encode($ques);
-         
-          
+		
+		
+		  
+			$count = count($ques);
+			$testcase = [];
+		 
+			for($i = 0; $i < $count; $i++){
+			
+			$cases = "SELECT case1,case2,case3,case4,answer1,answer2,answer3,answer4 FROM QuestionBank WHERE question = '$ques[$i]' ";
+			
+						$result2 = mysqli_query($connection, $cases);
+			
+			while($row2 = mysqli_fetch_assoc($result2)){
 
+            array_push($testcase, [
+            array($row2['case1']),
+			array($row2['case2']),
+			array($row2['case3']),
+			array($row2['case4']),
+			array($row2['answer1']),
+			array($row2['answer2']),
+			array($row2['answer3']),
+			array($row2['answer4'])
+    
+			]);
+          }
+			}
+			
+			
+	
+			
+			
+			
+			
+			
+			
 
+		  echo json_encode($ques);
+		  echo json_encode($testcase);
+		
               mysqli_close($connection);
 
 ?>
