14f6a7250d0006acdb18cbf690fb8418f3a40a7b
Austin <austinc726@gmail.com>
2017-04-27 00:14:55 -0400

    updated exam storing

--- examstore.php
+++ examstore.php
@@ -3,42 +3,40 @@
     
     include 'Connect.php';
 
-           $examID = $_POST['examID'];
-           $q = $_POST['newValue'];
-          // $encodeEx = json_encode($q);
-         
+        $examID = $_POST['examID'];
+		$ques = $_POST['ques']; 
+		
+		
            
                       
-            $e_query = mysqli_query($connection, "SELECT examid FROM Exam WHERE examid = '$examID' "); 
+            $e_query = mysqli_query($connection, "SELECT examid, examquestion FROM Exam WHERE examid = '$examID' AND examquestion = '$ques'"); 
             $returnCount = mysqli_num_rows($e_query);
             
                if($returnCount > 0) {
                
-                   $update_exam = "UPDATE Exam SET examquestion = '$q' WHERE examid = '$examID' ";
+                  echo "Question already part of exam id . $examID ";
                    
                }
                else {
             
-           
-                       $add_exam = "INSERT INTO `ac482`.`Exam` (`examquestion`, `examid`) VALUES ('$q', '$examID')";
-
+					
+						
+                       $add_exam = "INSERT INTO `ac482`.`Exam` (`examid`, `examquestion`) VALUES ('$examID', '$ques')";
           
                
                }
-              
-                    if (mysqli_query($connection, $add_exam)) {
-                    
-                          $success = "exam submitted successfully.";
-                           json_encode($success);
-                                 
-                    } 
-                    elseif(mysqli_query($connection, $update_exam)){
-                        $successful = "exam updated successfully";
-                        json_ecode($successful);
+					if(mysqli_query($connection, $add_exam)){
+                        $success = "exam submitted successfully";
+                        echo json_encode($success); 
+				
+						
                     }
+                 
                     else {
                     
                           echo "Error: " . $add_exam . "<br>" . mysqli_error($connection);
+						  
+					
                           
                     }
                     
