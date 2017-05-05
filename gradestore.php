88590a7018070a1f0f7a9f9362a942aa1da7ca8e
Austin <austinc726@gmail.com>
2017-05-04 20:05:31 -0400

    Update gradestore.php

--- gradestore.php
+++ gradestore.php
@@ -2,51 +2,29 @@
 <?php
     
     include 'Connect.php';
-    
-    
-
-           $grade = $_POST['grade'];
-           $explain = $_POST['feedback'];
-           
-         
-           
-                      
-            $g_query = mysqli_query($connection, "SELECT explanation FROM StudentGrades WHERE explanation = '$explain'"); 
-            $returnCount = mysqli_num_rows($g_query);
-            
-            
-            if(returnCount > 0 )
-            {
-            
-                  echo "Exam already graded";            
-            }
-            
-            
-           else {
-           
-           $add_grade = "INSERT INTO `ac482`.`StudentGrades` (`examgrade`, `explanation`) VALUES ('$grade', '$explain')";
-
-           }
-              
-                    if (mysqli_query($connection, $add_grade)) {
-                    
-                          $success = "Grade submitted successfully.";
-                          echo json_encode($success);
-                          
-                          echo $grade;
-                          echo $explain;
-                    
-                                 
-                    }
-                    
-                    else {
-                    
-                          echo "Error: " . $add_grade . "<br>" . mysqli_error($connection);
-                          
-                    }
-                    
-                    
-                   
+	
+	
+	$getfeedback = json_decode(file_get_contents("php://input"), true);
+	$question = $getfeedback["question"];
+	$value = $getfeedback["value"];
+	$questionpoints = $getfeedback["questionpoints"];
+	$compilefeedback = $getfeedback["compilefeedback"];
+	$compilepoints = $getfeedback["compilepoints"];
+	$casefeedback = $getfeedback["casefeedback"];
+	$casepoints = $getfeedback["casepoints"];
+	$parenbracketfeedback = $getfeedback["parenbracketfeedback"];
+	$parenbracketpoints = $getfeedback["parenbracketpoints"];
+	$grade = $getfeedback["grade"];
+	
+	
+	$g_query = "INSERT INTO `ac482`.`GradedExams` (`question`, `value`, `questionscore`, `compilestatus`, `compilegrade`, `casestatus`, `casegrade`, `parenbracketstatus`, `parenbracketgrade`, `grade`) 
+				  VALUES ('$question', '$value', '$questionpoints', '$compilefeedback', '$compilepoints', '$casefeedback', '$casepoints', '$parenbracketfeedback', '$parenbracketpoints', '$grade')";
+	
+	if(mysqli_query($connection, $g_query)){
+		echo "Graded exam stored successfully";
+	}
+	
+	
                    
                          mysqli_close($connection);
 ?>
