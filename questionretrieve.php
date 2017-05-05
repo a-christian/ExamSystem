0002e03f19cb9941db890fe98ec8b7e040ff97f1
Austin <austinc726@gmail.com>
2017-05-04 20:06:24 -0400

    Update questionretrieve.php

--- questionretrieve.php
+++ questionretrieve.php
@@ -1,52 +1,54 @@
 
 <?php
     
-    
-  include 'Connect.php';
-  
- 
+    include 'Connect.php';
 
-  $getQuestions = "SELECT * FROM QuestionBank";
-  
-
-  
-  
-  $result = mysqli_query($connection, $getQuestions);
-  
-  $numrows = mysqli_num_rows($result);
-
-  
-		$i = 0;
-	   
-     $exquest = [];
-      $exquest["questcount"] = $numrows;
-
-
-          while($row = mysqli_fetch_assoc($result)){
-			array_push($exquest, [
+            $methodName = $_POST['methodName'];
+            $pointValue = $_POST['pointValue'];
+            $category = $_POST['category'];
+            $difficulty = $_POST['difficulty'];
+            $question = $_POST['question'];
+            $parameter1 = $_POST['parameter1'];
+            $parameter2 = $_POST['parameter2'];
+            $parameter3 = $_POST['parameter3'];
+			$parameter4 = $_POST['parameter4'];
+            $case1 = $_POST['case1'];
+			$case2 = $_POST['case2'];
+			$case3 = $_POST['case3'];
+			$case4 = $_POST['case4'];
+			$answer1 = $_POST['answer1'];
+            $answer2 = $_POST['answer2'];
+			$answer3 = $_POST['answer3'];
+			$answer4 = $_POST['answer4'];
 			
-      array($row['question']),
-			array($row['pointValue']),
-			array($row['category']),
-			array($row['difficulty']),
-			array($row['case1']),
-			array($row['case2']),
-			array($row['case3']),
-			array($row['case4']),
-			array($row['answer1']),
-			array($row['answer2']),
-			array($row['answer3']),
-			array($row['answer4'])
 			
-            ]);
-			     
-
-          }     
-  
-			echo json_encode($exquest);
- 
-		  
-
-
-              mysqli_close($connection);
-?>  
+            $q_query = mysqli_query($connection, "SELECT methodName, category FROM QuestionBank WHERE question = '$methodName' AND category = '$category' "); 
+            $returnCount = mysqli_num_rows($q_query);
+            
+               if($returnCount > 0){
+                   echo "Question already exists in the database.";
+               }
+               else{
+                  $add_question = "INSERT INTO `ac482`.`QuestionBank` (`methodName`, `pointValue`, `category`, `difficulty`, `question`, `parameter1`, `parameter2`, `parameter3`, `parameter4`, `case1`, `case2`, `case3`, `case4`, `answer1`, `answer2`, `answer3`, `answer4`) 
+				  VALUES ('$methodName', '$pointValue', '$category', '$difficulty', '$question', '$parameter1', '$parameter2', '$parameter3', '$parameter4', '$case1', '$case2', '$case3', '$case4', '$answer1', '$answer2', '$answer3', '$answer4')";
+                  
+                  
+                                
+               }
+              
+              
+              
+                    if (mysqli_query($connection, $add_question)) {
+                          $success = "New question added to the bank!";
+                          json_encode($success);
+                          
+                    } 
+                    else {
+                          echo "Error: " . $add_question . "<br>" . mysqli_error($connection);
+                    }
+                    
+                    
+                   
+                   
+                         mysqli_close($connection);
+?>
