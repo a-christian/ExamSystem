61ed57ce00531c94d12c3b9e639b8019da248a53
Austin <austinc726@gmail.com>
2017-05-05 00:11:23 -0400

    Add files via upload

---
+++ retrieveupdatedgrade.php
@@ -0,0 +1,48 @@
+
+<?php
+    
+    
+  include 'Connect.php';
+  
+	  
+	  
+	  
+	  $getupdatedGrade = "SELECT * FROM UpdatedGradedExams";
+   
+	  $result = mysqli_query($connection, $getupdatedGrade);
+  
+      $numrows = mysqli_num_rows($result);
+
+  
+
+		$updatedfeedback = [];
+		$i = 0;
+		
+          	while($row = mysqli_fetch_assoc($result)){
+
+            array_push($updatedfeedback, [
+			unserialize($row['question']),
+			unserialize($row['answer']),
+			unserialize($row['questionScore']),
+			unserialize($row['compileStatus']),
+			unserialize($row['compileGrade']),
+			unserialize($row['caseStatus']),
+			unserialize($row['caseGrade']),
+			unserialize($row['parenBracket']),
+			unserialize($row['bracketGrade']),
+			$row['grade'],
+			$row['feedback']
+    
+			]);
+          }
+        //echo json_encode($updatedfeedback);
+		
+		print_r($updatedfeedback);
+		
+		  
+		 
+		  
+
+
+              mysqli_close($connection);
+?>  
